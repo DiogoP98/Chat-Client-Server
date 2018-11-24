460c1969431dd471f1fe79ab1e8eef8b39567326
FredyR4zox <frederico.almeida@live.com.pt>
2018-11-24 18:28:39 +0000

    Added auxiliary files

---
+++ Common.java
@@ -0,0 +1,11 @@
+public class Common {
+	static final public String ANS_MESSAGE	= "MESSAGE";
+    static final public String ANS_NEWNICK	= "NEWNICK";
+    static final public String ANS_JOINED	= "JOINED";
+    static final public String ANS_LEFT		= "LEFT";
+    static final public String ANS_PRIVATE	= "PRIVATE";
+
+    static final public String ANS_OK		= "OK\n";
+    static final public String ANS_ERROR	= "ERROR\n";
+    static final public String ANS_BYE		= "BYE\n";
+}
---
+++ User.java
@@ -0,0 +1,56 @@
+import java.nio.*;
+import java.nio.charset.*;
+    
+public class User {
+    private State state;
+    private String name;
+    private String room;
+    private String buffer;
+
+    static enum State {
+        INIT,
+        OUTSIDE,
+        INSIDE
+    }
+    
+    User() {
+        state = State.INIT;
+        name = null;
+        room = null;
+        buffer = "";
+    }
+
+    State getState() {
+        return state;
+    }
+
+    void setState(State state) {
+        this.state = state;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    String getRoom() {
+        return room;
+    }
+
+    void setRoom(String room) {
+        this.room = room;
+    }
+
+    void cacheMessage(String msg) {
+        buffer += msg;
+    }
+
+    String getFullMessage() {
+        String r = buffer;
+        buffer = "";
+        return r;
+    }
+}
